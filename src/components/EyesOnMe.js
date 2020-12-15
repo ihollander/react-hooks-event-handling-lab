dda468eec5448a09dbced6b1bb624423618ef5ec
ihollander <ianholla@gmail.com>
2020-12-15 09:32:08 -0500

    commit tree 03806b124c58839416834705fc09fae30a76d274

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(props) {
+  return (
+    <button
+      onBlur={() => console.log("Hey! Eyes on me!")}
+      onFocus={() => console.log("Good!")}
+    >
+      EyesOnMe
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad(props) {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+  return (
+    <div>
+      <input type="password" onChange={handleChange} />
+    </div>
+  );
+}
+
+export default Keypad;
